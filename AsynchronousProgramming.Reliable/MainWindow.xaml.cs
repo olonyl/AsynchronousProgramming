2d5cc609b6f8c69b77858145fdb8d44f30cb3957
olonyl <olony.rocha@contollo.net>
2022-02-21 15:13:58 -0600

    Add project files.

---
+++ AsynchronousProgramming.Reliable/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+
+namespace AsynchronousProgramming.Reliable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int count = 1;
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void btnDowload_Click(object sender, RoutedEventArgs e)
+        {
+            btnDowload.IsEnabled = false;
+
+            var client = new WebClient();
+
+            client.DownloadStringAsync(new Uri("https://feeds.feedburner.com/fekberg"));
+
+            client.DownloadStringCompleted += Client_DownloadStringCompleted;
+        }
+
+        private void Client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            txtDownload.Text = e.Result;
+            btnDowload.IsEnabled = true;
+        }
+
+        private void btnIncreaseCounter_Click(object sender, RoutedEventArgs e)
+        {
+            txtCounter.Text = $"Counter: {count++}";
+        }
+    }
+}
---
+++ AsynchronousProgramming.Unreliable/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Threading;
+using System.Windows;
+
+namespace AsynchronousProgramming.Unreliable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int count = 1;
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void btnDowload_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new WebClient();
+
+            var data = client.DownloadString("https://feeds.feedburner.com/fekberg");
+
+            Thread.Sleep(10000);
+
+            txtDownload.Text = data;
+        }
+
+        private void btnIncreaseCounter_Click(object sender, RoutedEventArgs e)
+        {
+            txtCounter.Text = $"Counter: {count++}";
+        }
+    }
+}
