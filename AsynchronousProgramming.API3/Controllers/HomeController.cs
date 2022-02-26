41faf04d1b8a4a34a27e487f4ceb09d8b599004f
olonyl <olony.rocha@contollo.net>
2022-02-25 22:50:10 -0600

    Added Web API Project to Implement Async calls

---
+++ AsynchronousProgramming.API3/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AsynchronousProgramming.API3.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public async Task<IHttpActionResult> Get()
+        {
+            using (var client = new HttpClient())
+            {
+                var httpMessage = await client.GetAsync("https://feeds.feedburner.com/fekberg");
+
+                var content = await httpMessage.Content.ReadAsStringAsync();
+
+                return Ok(content);
+            }
+
+        }
+    }
+}
---
+++ AsynchronousProgramming.API3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AsynchronousProgramming.API3.WebApiApplication" Language="C#" %>
